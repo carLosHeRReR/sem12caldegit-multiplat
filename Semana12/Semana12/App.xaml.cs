2b99ca134d92cfea6b06ce05f5fa70ca94bd4078
pachasp <elovfdhfshx475@gmail.com>
2023-06-07 21:10:29 -0500

    Agregar archivos de proyecto.

---
+++ Semana12/Semana12/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Semana12.ViewModel;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana12
+{
+    public partial class App : Application
+    {
+        CalculatorDemoViewModel calculatorDemoViewModel;
+        public App()
+        {
+            InitializeComponent();
+
+            calculatorDemoViewModel = new CalculatorDemoViewModel();
+            calculatorDemoViewModel.RestoreState(Current.Properties);
+
+            MainPage = new MainPage(calculatorDemoViewModel);
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+            calculatorDemoViewModel.SaveState(Current.Properties);
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Semana12/Semana12/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Semana12.ViewModel;
+using System;
+using Xamarin.Forms;
+
+namespace Semana12
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(CalculatorDemoViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel;
+        }
+
+        void OnPageSizeChanged(object sender, EventArgs args)
+        {
+            if (Width < Height)
+            {
+                mainGrid.RowDefinitions[1].Height = GridLength.Auto;
+                mainGrid.ColumnDefinitions[1].Width = new GridLength(0, GridUnitType.Absolute);
+
+                Grid.SetRow(keypadGrid, 1);
+                Grid.SetColumn(keypadGrid, 0);
+            }
+            else
+            {
+                mainGrid.RowDefinitions[1].Height = new GridLength(0, GridUnitType.Absolute);
+                mainGrid.ColumnDefinitions[1].Width = GridLength.Auto;
+                Grid.SetRow(keypadGrid, 0);
+                Grid.SetColumn(keypadGrid, 1);
+            }
+        }
+    }
+}
